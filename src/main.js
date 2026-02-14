2b8a1e9cbcfd74674b1c0f8f356c64dd5ac7b077
gagio <gagioia50@gmail.com>
2026-02-14 19:33:42 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import Stats from 'three/examples/jsm/libs/stats.module.js'
+import {GUI} from 'three/examples/jsm/libs/lil-gui.module.min.js'
+import {World} from './world';
+
+const gui = new GUI();
+
+const stats = new Stats();
+stats.dom.style.cssText = 'position:absolute;top:225px;right:0px;';
+document.body.appendChild(stats.dom);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls(camera, renderer.domElement)
+
+const world = new World(10, 10);
+scene.add(world);
+
+const sun = new THREE.DirectionalLight();
+sun.intensity = 3;
+sun.position.set(1, 2, 3);
+scene.add(sun)
+
+const ambient= new THREE.AmbientLight();
+ambient.intensity = 1;
+scene.add(ambient)
+
+camera.position.set(10, 2, 10);
+controls.update()
+
+function animate() {
+  controls.update();
+  renderer.render( scene, camera );
+  stats.update();
+}
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const worldFolder = gui.addFolder('World');
+worldFolder.add(world, 'width', 1, 20, 1).name('Width');
+worldFolder.add(world, 'height', 1, 20, 1).name('Height');
+worldFolder.addColor(world, 'color').name('Color', 255);
+worldFolder.add(world, 'treeCount', 1, 100, 1).name('Tree Count');
+worldFolder.add(world, 'rockCount', 1, 100, 1).name('Rock Count');
+worldFolder.add(world, 'bushCount', 1, 100, 1).name('Bush Count');
+
+worldFolder.add(world, 'generate').name('Generate');
+
